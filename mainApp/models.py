8bbde71fdbba75f7f07c13b2a9f5765287a0c63c
Gaurav Jain <crazcuber@gmail.com>
2018-07-01 21:29:49 +0530

    added client and token

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Client(models.Model):
+    name = models.CharField(max_length = 50,null=False)
+    client_id = models.CharField(max_length = 255,null=False)
+    client_secret = models.CharField(max_length = 255,null=False)
+    client_type = models.CharField(max_length = 50,null=False)
+    
+class AuthToken(models.Model):
+    token = models.CharField(max_length = 255,null=False)
+    expires = models.IntegerField(default = 200)
+    added = models.DateTimeField(auto_now_add = True)
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name = 'User',
+        null = False,
+        blank = False,
+    )
+    Client = models.ForeignKey(
+        Client,
+        on_delete=models.CASCADE,
+        verbose_name = 'User',
+        null = False,
+        blank = False,
+    )
+    revoked = models.BooleanField(default = False)
--- mainApp/views.py
+++ mainApp/views.py
@@ -3,9 +3,11 @@ from django.http import JsonResponse
 # Create your views here.
 def index(request):
     params = {
-        'somerthin':'value',
+        'somerthin':{
+            'key':'value'
+        },
         'somerthin1':'value',
         'somerthin2':'value',
         'somerthin3':'value',
     }
-    return JsonResponse(parms)
+    return JsonResponse(params)
