eaa5616588753ecd038a4050ec5f457691c9d67b
Gaurav Jain <crazcuber@gmail.com>
2018-07-02 23:21:45 +0530

    authenticate

--- mainApp/views.py
+++ mainApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import *
+from django.contrib.auth import authenticate, login
 
 @api_view(['POST'])
 def token(request):
